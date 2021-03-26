4277526724363d89006494cd50954bd9837f86cb
carlosfluna <carlosfluna@misena.edu.co>
2021-03-25 21:27:05 -0500

    crecion objetos, clase operacion

--- Prueba_Git/Models/Usuarios.cs
+++ Prueba_Git/Models/Usuarios.cs
@@ -7,5 +7,8 @@ namespace Prueba_Git.Models
 {
     public class Usuarios
     {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string email { get; set; }
     }
 }
